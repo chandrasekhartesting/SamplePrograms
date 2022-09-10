ad691fddac3b0ed5ea552d091dc3243a2259cffe
chandrasekhartesting <112968281+chandrasekhartesting@users.noreply.github.com>
2022-09-10 14:27:59 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,14 +4,17 @@ import java.io.*;
 
 class Hello
 {
+	public static double pi = 3.14;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		System.out.println("Hello "+args[0]+"!");
+		int number = 2;
 		add(5,6);
 		add(4,7);
 		System.out.println("sum :"+add(4,6).toString());
 	}
 	public static int add(int a,int b){
-		return a+b;
+		int c = a + b;
+		return c;
 	}
 }
