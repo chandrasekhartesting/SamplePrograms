67f1235808e08c20115f4fcf1c92f5e663fa65f4
chandrasekhartesting <112968281+chandrasekhartesting@users.noreply.github.com>
2022-09-15 14:55:51 +0530

    by Hasini

--- Hello.java
+++ Hello.java
@@ -9,10 +9,7 @@ class Hello
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		System.out.println("Hello "+args[0]+"!");
-		int number = 2;
-		add(5,6);
-		add(4,7);
-		System.out.println("sum :"+add(4,6).toString());
+		
 	}
 	public static int add(int a,int b){
 		int c = a + b;
